35977a7d3e4de11109656d7a58dcf3e426b243aa
Panupong <panupong.not@hotmail.com>
2019-01-15 00:31:32 +0700

    file check fix bug and clear state

--- src/FlieUploader.tsx
+++ src/FlieUploader.tsx
@@ -17,14 +17,29 @@ export class FileUploader extends React.Component<Props> {
 
     handleFileRead(ev: ProgressEvent) {
         const content = this.reader.result as string;
-        const data = content.split('\n').map((d) => JSON.parse(d) as DataTypes.Data);
-        this.props.onReceiveData(data);
-        console.log(data);
+        const data = content.trim().split('\n').filter((d) => d.trim().length > 0);
+        console.log('Input Data :', data);
+
+        const parseData = data.map((d) => {
+            try {
+                const parseD = JSON.parse(d) as DataTypes.Data;
+                return parseD;
+            } catch {
+                return {} as DataTypes.Data;
+            }
+        }).filter((d) => Object.keys(d).length > 0);
+        this.props.onReceiveData(parseData);
     }
 
     receiveFile(e: React.FormEvent<HTMLInputElement>) {
         let file = e.currentTarget.files && e.currentTarget.files[0];
-        this.reader.readAsText(file as Blob);
+        console.log(file);
+        const fileExtensionRegex = /^.*\.(json|jsonl)$/i;
+        if (file && fileExtensionRegex.test(file.name)) {
+            this.reader.readAsText(file as Blob);
+        } else {
+            alert('File not support!!');
+        }
     }
 
     render() {
---
+++ src/Table/ExpandableRow.tsx
@@ -0,0 +1,30 @@
+import { ReactElement, PureComponent } from 'react'
+
+interface Props {
+    cols: string[];
+    expandElement: ReactElement<HTMLDivElement>;
+}
+
+interface States {
+    isExpand: boolean;
+}
+export class ExpandableRow extends PureComponent<Props, States> {
+
+    constructor(props: Props, states: States) {
+        super(props);
+        this.state = { isExpand: false }
+    }
+
+    onRowClick() {
+        this.setState({ isExpand: !this.state.isExpand })
+    }
+
+    render() {
+        return (
+            <>
+                <tr onClick={this.onRowClick}>{this.props.cols.map(col => <td>{col}</td>)}</tr>
+                <td>{this.props.expandElement}</td>
+            </>
+        );
+    }
+}
